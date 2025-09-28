ccb0f116ae1222049767c9480c9c0ae06c8f803e
Emanuele Zignale <zignale92@gmail.com>
2025-09-28 12:46:24 +0200

    linspace sum mean std

---
+++ linspace.py
@@ -0,0 +1,10 @@
+import numpy as np
+
+arr = np.linspace(0, 10, 5)
+print(arr)
+
+# Output: [ 0.   2.5  5.   7.5 10. ]
+
+random_arr = np.random.rand(3, 4)
+print(random_arr)
+# Output: Matrice 3x4 con valori casuali tra 0 e 1
