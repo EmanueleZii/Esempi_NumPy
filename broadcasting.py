5483cf042f74021851cb74d91c56b2f29483a293
Emanuele Zignale <zignale92@gmail.com>
2025-09-29 09:47:20 +0200

    broadcasting

--- broadcasting.py
+++ broadcasting.py
@@ -1,13 +1,11 @@
 import numpy as np
 
-
 arr = np.array([1, 2, 3, 4])
 
 scalar = 10
 
 result = arr + scalar
 print(result)  # Output: [11 12 13 14]
-
 
 a = np.array([1, 2, 3])
 b = np.array([4,5,6])
